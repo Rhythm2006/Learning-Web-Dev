50cb7d9fed5f6cd9832f3edad5c4625dadc5dc69
Rhythm <rithimesh@gmail.com>
2026-03-25 23:21:23 +0530

    Starting with Routing commit 1

---
+++ week9-spa's_and_routing/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom'
+import './App.css'
+// there are two ways to navigate one by LINK which is a component or usenavigate which is a hook
+// You might wanna use LINK when the user specifically clicks on a button like in the following example and the use case of usenavigate is for some custom logic 
+function App() {
+  
+
+  return (
+    <>
+    <BrowserRouter>
+    <Link to="/">Allen</Link> | <Link to="/neet/online-class-11-coaching">Class 11 </Link> || <Link to="/neet/online-class-12-coaching">Class 12 </Link> || <Link to="/neet/online-class-13-coaching">Dropper Batch</Link>
+    <Routes>
+      <Route path="/neet/online-class-11-coaching" element={<Class11Program />}/>
+      <Route path="/neet/online-class-12-coaching" element={<Class12Program />}/>
+      <Route path="/neet/online-class-13-coaching" element={<Class13Program />}/>
+      <Route path="/" element={<Homepage/>}/>
+
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+
+
+function Class11Program(){
+  const navigate = useNavigate();
+  function redirectUser(){
+    navigate("/");
+  }
+  return (
+    <div>
+  Class 11 Neet Coaching <br/> <button onClick={redirectUser}>Go Back to landing page</button>
+  </div>
+  )
+  
+}
+function Class12Program(){
+  return (
+    <div>
+  Class 12 Neet Coaching 
+  </div>
+  )
+  
+}
+function Class13Program(){
+  return (
+    <div>
+  Class 13 Neet Coaching 
+  </div>
+  )
+  
+}
+function Homepage(){
+  return (
+    <div>
+  Welcome to India's best Coaching classes!
+  </div>
+  )
+  
+}
+
+export default App
