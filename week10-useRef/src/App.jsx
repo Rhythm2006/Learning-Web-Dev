473e19463f33498e8e3c0356eef5efc5ab34cf4e
Rhythm <rhythsanjeev2006@gmail.com>
2026-03-26 20:26:22 +0530

    feat: implemented basic useRef example for input focus handling

---
+++ week10-useRef/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState, useRef} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+// In react useRef is a hook that is way to create a refernce to a value or DOM that persists across a render but doesnt trigger a rerender.
+//The value of ref doesnt reset after each rerender
+//Changing the value of ref doesnt trigger a rerender
+//Some usecases 1: Focusing an input box(accessing dom elements )
+
+function App() {
+  const inputRef = useRef();
+  function focusInput(){
+    inputRef.current.focus()
+  }
+  return (
+    <>
+     <input ref={inputRef} type='text' placeholder='Kindly type something'/>
+     <button onClick={focusInput}>Focus The Input</button>
+    </>
+  )
+}
+
+export default App
